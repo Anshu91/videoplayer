b6ba1a115bfde04ebf1558c5cb3b11325264d485
Anshu91 <kumaranshu888@gmail.com>
2020-08-08 21:22:18 +0530

    Updates

---
+++ precache-manifest.6cc2b6a3dfc07c5dbdb43169316ca31d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c407e2423ec70babd89b24325863bd61",
+    "url": "/videoplayer/index.html"
+  },
+  {
+    "revision": "9c7a5fe46a84672c12ae",
+    "url": "/videoplayer/static/css/main.2b32f5dd.chunk.css"
+  },
+  {
+    "revision": "bffd302de79a84af2cdd",
+    "url": "/videoplayer/static/js/2.a5aa5a07.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/videoplayer/static/js/2.a5aa5a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c7a5fe46a84672c12ae",
+    "url": "/videoplayer/static/js/main.101efc85.chunk.js"
+  },
+  {
+    "revision": "9ad6fb79de04b806383e",
+    "url": "/videoplayer/static/js/runtime-main.20877cb2.js"
+  }
+]);
